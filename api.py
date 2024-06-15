556909b0828359b9c0248fbedc2ba12fa6be8e3a
Eric Bratu <gymnaztv@gmail.com>
2024-06-14 20:40:44 -0400

    Add More Class Info to Output
    Now able to output the title of the course and the assignment titles associated with the due date of the assignments in a neat fashion

--- api.py
+++ api.py
@@ -4,15 +4,19 @@ from datetime import datetime, timedelta
 
 headers = {"Authorization": "Bearer " + "<INPUT CANVAS API KEY HERE!>"}
 
-response = requests.get('<INPUT URL REQUEST HERE!>', headers=headers)
+class1_info = requests.get('<INPUT URL REQUEST FOR GET A SINGLE COURSE UNDER COURSES CATEGORY HERE!>', headers = headers)
+class1_assment = requests.get('<INPUT URL REQUEST FOR LIST ASSIGNMENTS FOR USER UNDER ASSIGNMENTS CATEGORY HERE!>', headers=headers)
 
 # Parse the JSON response
-assignments = response.json()
+class_info = class1_info.json()
+assignments = class1_assment.json()
 
-# Extract, adjust, and print the 'due_at' values
+course_code = class_info.get('course_code')
+print(f"{course_code}:")
+
+# Extract, adjust, and print 'due_at' values with 'name'
 for assignment in assignments:
-    if 'due_at' in assignment:
         due_at_str = assignment['due_at'][:10]
         due_at_date = datetime.strptime(due_at_str, '%Y-%m-%d')
-        adjusted_date = due_at_date - timedelta(days=1)
-        print(adjusted_date.strftime('%Y-%m-%d'))
+        adjusted_date = due_at_date - timedelta(days=1) # Date naturally output is wrong by +1 day for some reason, just subtracting one day to get correct due date
+        print(f"{assignment['name']}:",adjusted_date.strftime('%Y-%m-%d'),"\n")
