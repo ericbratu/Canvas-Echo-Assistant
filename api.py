d81971e578e66ed2652f24737c87ba7cf83aba95
Eric Bratu <gymnaztv@gmail.com>
2024-06-14 23:59:11 -0400

    now able to handle multiple classes!

--- api.py
+++ api.py
@@ -2,21 +2,36 @@ import requests
 import json
 from datetime import datetime, timedelta
 
+
 headers = {"Authorization": "Bearer " + "<INPUT CANVAS API KEY HERE!>"}
 
-class1_info = requests.get('<INPUT URL REQUEST FOR GET A SINGLE COURSE UNDER COURSES CATEGORY HERE!>', headers = headers)
-class1_assment = requests.get('<INPUT URL REQUEST FOR LIST ASSIGNMENTS FOR USER UNDER ASSIGNMENTS CATEGORY HERE!>', headers=headers)
 
-# Parse the JSON response
-class_info = class1_info.json()
-assignments = class1_assment.json()
+# Now can handle multiple classes :D
+class_urls = [
+    ('<INPUT URL REQUEST FOR GET A SINGLE COURSE UNDER COURSES CATEGORY HERE!>', '<INPUT URL REQUEST FOR LIST ASSIGNMENTS FOR USER UNDER ASSIGNMENTS CATEGORY HERE!>'),
+    ('<INPUT URL REQUEST FOR GET A SINGLE COURSE UNDER COURSES CATEGORY HERE!>', '<INPUT URL REQUEST FOR LIST ASSIGNMENTS FOR USER UNDER ASSIGNMENTS CATEGORY HERE!>')
+]
 
-course_code = class_info.get('course_code')
-print(f"{course_code}:")
 
-# Extract, adjust, and print 'due_at' values with 'name'
-for assignment in assignments:
-        due_at_str = assignment['due_at'][:10]
-        due_at_date = datetime.strptime(due_at_str, '%Y-%m-%d')
-        adjusted_date = due_at_date - timedelta(days=1) # Date naturally output is wrong by +1 day for some reason, just subtracting one day to get correct due date
-        print(f"{assignment['name']}:",adjusted_date.strftime('%Y-%m-%d'),"\n")
+for class_info_url, class_assments_url in class_urls:
+    # Now broader instead of specific to each class. Retrieves class info and respective assignments
+    class_info = requests.get(class_info_url, headers=headers).json()
+    class_assments = requests.get(class_assments_url, headers=headers).json()
+
+
+    # Extract the class code from class info
+    course_code = class_info.get('course_code')
+
+
+    # Print the course code
+    print(f"{course_code}:")
+
+
+# Extract, adjust, and print 'due_at' values with associated assignment title
+    for assignment in class_assments:
+        if 'due_at' in assignment:
+            due_at_str = assignment['due_at'][:10]
+            due_at_date = datetime.strptime(due_at_str, '%Y-%m-%d')
+            adjusted_date = due_at_date - timedelta(days=1)  # Date naturally output is wrong by +1 day for some reason, just subtracting one day to get correct due date
+            print(f"{assignment['name']}:\n{adjusted_date.strftime('%m-%d-%Y')}\n")
+
